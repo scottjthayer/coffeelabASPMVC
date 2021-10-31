4f15429760c8e08cdec53a2e62c41566b94bc792
scott <scottjthayer@gmail.com>
2021-10-30 21:42:38 -0400

    first commit for ASP.NET coffee lab

---
+++ Coffee Shop Registration LAB/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Coffee_Shop_Registration_LAB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee_Shop_Registration_LAB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Summary(string firstName, string lastName, string personality )
+        {
+            ViewData["Summary"] = $"Thank you {firstName} {lastName}! You are {personality} and have signed up for our coffee club!";
+            
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
